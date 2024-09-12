75ff59b7829200f5ce0933fbc52f4a1cf217c4bf
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-12 13:27:25 +0300

    add dartz package

--- lib/Features/home/Domain/repos/home_repo.dart
+++ lib/Features/home/Domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_clean_arch/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+
 import '../entities/bookly_entity.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewSetBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewSetBooks();
 }
