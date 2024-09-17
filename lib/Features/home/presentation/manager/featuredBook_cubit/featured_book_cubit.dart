7846a9d81faf932ea5dac895a6c666b167e4269a
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-17 19:00:42 +0300

    create featuredBook cubit states

---
+++ lib/Features/home/presentation/manager/featuredBook_cubit/featured_book_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+
+import '../../../Domain/entities/bookly_entity.dart';
+
+part 'featured_book_state.dart';
+
+class FeaturedBookCubit extends Cubit<FeaturedBookState> {
+  FeaturedBookCubit() : super(FeaturedBookInitial());
+}
---
+++ lib/Features/home/presentation/manager/featuredBook_cubit/featured_book_state.dart
@@ -0,0 +1,19 @@
+part of 'featured_book_cubit.dart';
+
+abstract class FeaturedBookState {}
+
+class FeaturedBookInitial extends FeaturedBookState {}
+
+class FeaturedBookLoading extends FeaturedBookState {}
+
+class FeaturedBookSuccess extends FeaturedBookState {
+  final List<BookEntity> books;
+
+  FeaturedBookSuccess(this.books);
+}
+
+class FeaturedBookFailure extends FeaturedBookState {
+  final String errorMessage;
+
+  FeaturedBookFailure(this.errorMessage);
+}
