513ab46f38789cb5d7143a196120076b4d6a19b7
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-18 18:49:35 +0300

    impl NewsetBooksCubit

--- lib/Features/home/presentation/manager/NewsetBooks_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/NewsetBooks_cubit/newset_books_cubit.dart
@@ -1,8 +1,17 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import '../../../Domain/entities/bookly_entity.dart';
+import '../../../Domain/use_case/fetch_newset_books_use_case.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.fetchNewsetBooksUseCase) : super(NewsetBooksInitial());
+  final FetchNewsetBooksUseCase fetchNewsetBooksUseCase;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await fetchNewsetBooksUseCase.call();
+    result.fold((l) => emit(NewsetBooksFailure(l.toString())),
+        (r) => emit(NewsetBooksSucsses(r)));
+  }
 }
