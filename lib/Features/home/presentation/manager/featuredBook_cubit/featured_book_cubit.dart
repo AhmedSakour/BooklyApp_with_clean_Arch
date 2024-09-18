0ce2d92f965c5ee78ffa837bed62bac8b330c651
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-18 18:40:10 +0300

    impl featuredBooksCubit

--- lib/Features/home/presentation/manager/featuredBook_cubit/featured_book_cubit.dart
+++ lib/Features/home/presentation/manager/featuredBook_cubit/featured_book_cubit.dart
@@ -1,9 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_clean_arch/Features/home/Domain/use_case/fetch_featuredBook_use_case.dart';
 
 import '../../../Domain/entities/bookly_entity.dart';
 
 part 'featured_book_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.featuredBooksUseCase) : super(FeaturedBookInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBookLoading());
+    var result = await featuredBooksUseCase.call();
+    result.fold((l) => emit(FeaturedBookFailure(l.toString())),
+        (r) => emit(FeaturedBookSuccess(r)));
+  }
 }
