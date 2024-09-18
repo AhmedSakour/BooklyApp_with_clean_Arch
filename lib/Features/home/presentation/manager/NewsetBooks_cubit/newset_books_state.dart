5c8764472fcbb38e968dab166b6f7ff222f2f0ad
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-18 18:47:42 +0300

    create NewsetBooks cubit states

---
+++ lib/Features/home/presentation/manager/NewsetBooks_cubit/newset_books_state.dart
@@ -0,0 +1,19 @@
+part of 'newset_books_cubit.dart';
+
+abstract class NewsetBooksState {}
+
+class NewsetBooksInitial extends NewsetBooksState {}
+
+class NewsetBooksLoading extends NewsetBooksState {}
+
+class NewsetBooksSucsses extends NewsetBooksState {
+  final List<BookEntity> books;
+
+  NewsetBooksSucsses(this.books);
+}
+
+class NewsetBooksFailure extends NewsetBooksState {
+  final String errorMessage;
+
+  NewsetBooksFailure(this.errorMessage);
+}
