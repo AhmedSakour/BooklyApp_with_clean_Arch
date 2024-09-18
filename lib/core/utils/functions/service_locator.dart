cf2703fab9cf4753b445a79c39f33cf94cd8bf9c
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-18 19:37:25 +0300

    provide cubit to main file

---
+++ lib/core/utils/functions/service_locator.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_clean_arch/Features/home/data/data%20_sources/home_local_data_source.dart';
+import 'package:bookly_clean_arch/Features/home/data/data%20_sources/home_remote_data_source.dart';
+import 'package:bookly_clean_arch/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_clean_arch/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(getIt.get<ApiService>())));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:bookly_clean_arch/Features/home/Domain/entities/bookly_entity.dart';
+import 'package:bookly_clean_arch/Features/home/Domain/use_case/fetch_newset_books_use_case.dart';
+import 'package:bookly_clean_arch/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_clean_arch/Features/home/presentation/manager/NewsetBooks_cubit/newset_books_cubit.dart';
+import 'package:bookly_clean_arch/core/utils/functions/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
+import 'Features/home/Domain/use_case/fetch_featuredBook_use_case.dart';
+import 'Features/home/presentation/manager/featuredBook_cubit/featured_book_cubit.dart';
 import 'constants.dart';
 import 'core/utils/app_router.dart';
 
@@ -19,12 +26,25 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBookCubit(
+              FetchFeaturedBooksUseCase(homeRepo: getIt.get<HomeRepoImpl>())),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBooksCubit(
+              FetchNewsetBooksUseCase(homeRepo: getIt.get<HomeRepoImpl>())),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
