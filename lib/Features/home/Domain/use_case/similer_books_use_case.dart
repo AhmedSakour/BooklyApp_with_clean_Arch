e966f601e60a87399e668d5ffd0ba5705932f5a6
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-22 12:51:19 +0300

    impl SimilerBooksCubit

--- lib/Features/home/Domain/use_case/similer_books_use_case.dart
+++ lib/Features/home/Domain/use_case/similer_books_use_case.dart
@@ -4,10 +4,10 @@ import 'package:bookly_clean_arch/core/error/failure.dart';
 import 'package:bookly_clean_arch/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class SimilerBookUseCase extends UseCase<List<BookEntity>, NoParams> {
+class FetchSimilerBookUseCase extends UseCase<List<BookEntity>, NoParams> {
   final HomeRepo homeRepo;
 
-  SimilerBookUseCase({required this.homeRepo});
+  FetchSimilerBookUseCase({required this.homeRepo});
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParams? param]) async {
     return await homeRepo.fetchSimilerBooks();
--- lib/Features/home/presentation/manager/similerBooks_cubit/similer_books_cubit.dart
+++ lib/Features/home/presentation/manager/similerBooks_cubit/similer_books_cubit.dart
@@ -1,8 +1,17 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly_clean_arch/Features/home/Domain/entities/bookly_entity.dart';
+import 'package:bookly_clean_arch/Features/home/Domain/use_case/similer_books_use_case.dart';
 
 part 'similer_books_state.dart';
 
 class SimilerBooksCubit extends Cubit<SimilerBooksState> {
-  SimilerBooksCubit() : super(SimilerBooksInitial());
+  SimilerBooksCubit(this.fetchSimilerBookUseCase)
+      : super(SimilerBooksInitial());
+  final FetchSimilerBookUseCase fetchSimilerBookUseCase;
+  Future<void> fetchSimilerBooks() async {
+    emit(SimilerBooksLoading());
+    var result = await fetchSimilerBookUseCase.call();
+    result.fold((l) => emit(SimilerBooksFailure(l.erroMessage)),
+        (r) => emit(SimilerBooksSuccess(r)));
+  }
 }
