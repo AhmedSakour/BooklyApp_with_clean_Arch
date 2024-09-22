607242d043494585504765cc4761fe17c4da9635
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-22 12:44:31 +0300

    create SimilerBooksCubit states

--- lib/Features/home/presentation/manager/similerBooks_cubit/similer_books_state.dart
+++ lib/Features/home/presentation/manager/similerBooks_cubit/similer_books_state.dart
@@ -3,3 +3,17 @@ part of 'similer_books_cubit.dart';
 abstract class SimilerBooksState {}
 
 class SimilerBooksInitial extends SimilerBooksState {}
+
+class SimilerBooksLoading extends SimilerBooksState {}
+
+class SimilerBooksSuccess extends SimilerBooksState {
+  final List<BookEntity> books;
+
+  SimilerBooksSuccess(this.books);
+}
+
+class SimilerBooksFailure extends SimilerBooksState {
+  final String errorMessage;
+
+  SimilerBooksFailure(this.errorMessage);
+}
