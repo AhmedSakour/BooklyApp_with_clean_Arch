fc6efb061b9fcb63062a19a15251fffc7c35da0a
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-24 13:50:19 +0300

    impl pagination in SimilerBook

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,23 +1,60 @@
 import 'package:bookly_clean_arch/Features/home/Domain/entities/bookly_entity.dart';
+import 'package:bookly_clean_arch/Features/home/presentation/manager/similerBooks_cubit/similer_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_item.dart';
 
-class SimilarBooksListview extends StatelessWidget {
+class SimilarBooksListview extends StatefulWidget {
   const SimilarBooksListview({super.key, required this.books});
   final List<BookEntity> books;
+
+  @override
+  State<SimilarBooksListview> createState() => _SimilarBooksListviewState();
+}
+
+class _SimilarBooksListviewState extends State<SimilarBooksListview> {
+  late final ScrollController _scrollController;
+  bool isLoading = false;
+  int pageNumber = 1;
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(scrollListener);
+    super.initState();
+  }
+
+  void scrollListener() async {
+    var currerntPostion = _scrollController.position.pixels;
+    var maxPostion = _scrollController.position.maxScrollExtent;
+    if (currerntPostion >= 0.7 * maxPostion) {
+      if (!isLoading) {
+        isLoading = true;
+        await BlocProvider.of<SimilerBooksCubit>(context)
+            .fetchSimilerBooks(pageNumber: pageNumber++);
+        isLoading = false;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .15,
       child: ListView.builder(
+          controller: _scrollController,
           scrollDirection: Axis.horizontal,
-          itemCount: books.length,
+          itemCount: widget.books.length,
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: CustomBookImage(image: books[index].image),
+              child: CustomBookImage(image: widget.books[index].image),
             );
           }),
     );
