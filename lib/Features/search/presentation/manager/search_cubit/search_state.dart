669e27923fdee04e439f192b19ca25fbfce945e1
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-27 14:56:18 +0300

    update SearchState

--- lib/Features/search/presentation/manager/search_cubit/search_state.dart
+++ lib/Features/search/presentation/manager/search_cubit/search_state.dart
@@ -6,6 +6,8 @@ class SearchInitial extends SearchState {}
 
 class SearchLoading extends SearchState {}
 
+class SearchLoadingPagination extends SearchState {}
+
 class SearchSuccess extends SearchState {
   final List<BookEntity> books;
 
@@ -16,4 +18,10 @@ class SearchFailure extends SearchState {
   final String errorMessage;
 
   SearchFailure(this.errorMessage);
+}
+
+class SearchFailurePagination extends SearchState {
+  final String errorMessage;
+
+  SearchFailurePagination(this.errorMessage);
 }
